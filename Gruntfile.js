04020eb5b1e92b65a09f7a31f9293e38855a0f4c
Jason Bishop <jasonbishop@macpros-Mac-Pro-2.local>
2015-07-03 14:34:31 +0100

    added grunt functionality

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+module.exports = function(grunt) {
+    var skinDir = 'magentotheme/skin/frontend/my-theme/default/';
+    var appDir = 'magentotheme/app/design/frontend/my-theme/default/';
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        compass: {
+            dist: {
+                options: {
+                    sassDir: skinDir + 'scss',
+                    cssDir: skinDir + 'css',
+                    environment: 'production',
+                    outputStyle: 'compressed'
+                }
+            }
+        },
+        clean: {
+            images: {
+                src: [skinDir + 'images']
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: skinDir + 'images-src/',
+                    src: '**/*.{png,jpg,gif}',
+                    dest: skinDir + 'images/'
+                }]
+            }
+        },
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                skinDir + ['js/{,*/}*.js', '!js/{,*/}*.min.js']
+            ]
+        },
+        uglify: {
+            dist: {
+                options: {
+                    mangle: false
+                },
+                files: {
+                    'theme/skin/frontend/my-theme/default/js/scripts.min.js': [skinDir + 'js/scripts.js']
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            livereload: {
+                files: [
+                    appDir + '**/*.{phtml,xml}',
+                    skinDir + 'scss/{,*/}*.scss',
+                    skinDir + 'images-src/{,*/}*.{png,jpg,gif}',
+                    [skinDir + 'js/*.js', '!' + skinDir + 'js/*.min.js']
+                ],
+                tasks: [
+                    'compass',
+                    'clean:images',
+                    'imagemin',
+                    'jshint',
+                    'uglify'
+                ]
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', [
+        'compass',
+        'clean:images',
+        'imagemin',
+        'jshint',
+        'uglify'
+    ]);
+};
